96449b6d26d07895c53ef57f610c6ce597c1717a
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-20 08:12:24 +0100

    delete function done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,12 +3,30 @@ import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const users = useLoaderData();
+    const handledelete = _id =>{
+        console.log('delete', _id);
+        fetch(`http://localhost:3000/users/${_id}`,{
+            method: 'DELETE',
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.deletedCount>0){
+                alert('deleted successfully');
+            }
+        })
+
+    }
     return (
         <div>
             <h2>Total Number of Available user :  {users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p
+                     key={user._id}>
+                        {user.name} : {user.email} : {user._id} <button 
+                        className="btn" onClick={() => handledelete(user._id)}
+                        >x</button></p>)
                 }
             </div>
         </div>
